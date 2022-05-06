94f92057454af6409e116f8bf79e9a25ff08c292
GinaDunlap <99146802+GinaDunlap@users.noreply.github.com>
2022-05-06 00:25:25 -0400

    Add files via upload

---
+++ seed.sql
@@ -0,0 +1,15 @@
+insert into users 
+    (name, email)
+values
+('Fred', 'FF@bedrock.com'),
+('Barney', 'BF@bedrock.com'),
+('Wilma' , 'WF@bedrock.com'),
+('Betty' , 'BR@bedrock.com'),
+('Pebbles' ,'PF@bedrock.com'),
+('George' , 'GJ@OrbitCity.com'),
+('Jane' , 'JJ@OrbitCity.com'),
+('Judy' , 'JJet@OrbitCity.com'),
+('Elroy', 'EJ@OrbitCity.com'),
+('Astro' , 'AJ@OrbitCity.com')
+;
+
